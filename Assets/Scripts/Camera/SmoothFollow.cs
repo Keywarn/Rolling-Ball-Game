2ba004c41ed4979d307edb47634d571ddac6b588
Keywarn <charliehasalamb@icloud.com>
2020-11-14 12:53:00 +0000

    Basic player movement in proto scene

---
+++ Assets/Scripts/Camera/SmoothFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    public class SmoothFollow : MonoBehaviour
+    {
+
+        // The target we are following
+        [SerializeField]
+        private GameObject player;
+        // The distance in the x-z plane to the target
+        [SerializeField]
+        private Vector3 offset;
+
+        // Use this for initialization
+        void Start() { 
+			offset = transform.position - player.transform.position;
+		}
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rigid;
+    
+
+    [SerializeField]
+    private float rollSpeed = 50;
+
+    [SerializeField]
+    private LayerMask ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //Check for ground
+        if(Grounded()) {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+
+             // Slow down when no input recieved
+            if (input.magnitude < 0.1f && rigid.velocity.magnitude > 0.0f){
+                rigid.velocity = Vector3.Lerp(rigid.velocity, Vector3.zero, rollSpeed * 0.1f * Time.deltaTime);
+            }
+            //Zoom
+            rigid.AddForce(input * rollSpeed);
+        }
+    }
+
+    public bool Grounded()
+    {
+        return Physics.CheckSphere(transform.position - (Vector3.up * 0.5f), 1, ground);
+    }
+}
