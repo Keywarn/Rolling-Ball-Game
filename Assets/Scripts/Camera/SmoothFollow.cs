8bc16a29b3083671ad0029777cfb075886f6cec4
Keywarn <charliehasalamb@icloud.com>
2020-12-26 19:54:38 +0000

    removed all stuff

--- Assets/Scripts/Camera/SmoothFollow.cs
+++ Assets/Scripts/Camera/SmoothFollow.cs
@@ -33,7 +33,7 @@ public class SmoothFollow : MonoBehaviour
 	}
 
 	void Update() {
-		if(!player.GetComponent<PlayerMove>().flying && player.GetComponent<PlayerMove>().Grounded() && !player.GetComponent<PlayerMove>().canScore){
+		if(player.GetComponent<PlayerMove>().Grounded()){
 			CameraTilt();
 		}
 	}
@@ -42,7 +42,7 @@ public class SmoothFollow : MonoBehaviour
 	{
 		// Rotate camera container along the x axis when tilting the joystick up or down to give a forward and back tilt effect.
 		// The further up the joystick is the higher the angle for target rotation will be and vice versa.
-		float scaledVerticalTilt = initialXRotation - (SimpleInput.GetAxis("Vertical") * maxVerticalAngle);
+		float scaledVerticalTilt = initialXRotation + (SimpleInput.GetAxis("Vertical") * maxVerticalAngle);
 
 		// Using floor normal adjust the rotation of the camera's x axis at rest.
 		float angleBetweenFloorNormal = useFloorNormal ? Vector3.SignedAngle(Vector3.up, player.GetComponent<PlayerMove>().GetFloorNormal(), transform.right) : 0.0f;
@@ -63,64 +63,28 @@ public class SmoothFollow : MonoBehaviour
 	// Update is called once per frame
 	void LateUpdate()
 	{
-		//Follow Script for not flying
-		if(!player.GetComponent<PlayerMove>().flying){
-			// Get forward vector minus the y component
-			Vector3 vectorA = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
+		// Get forward vector minus the y component
+		Vector3 vectorA = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
 
-			// Get target's velocity vector minus the y component
-			Vector3 vectorB = new Vector3(rigid.velocity.x, 0.0f, rigid.velocity.z);
+		// Get target's velocity vector minus the y component
+		Vector3 vectorB = new Vector3(rigid.velocity.x, 0.0f, rigid.velocity.z);
 
-			// Find the angle between vectorA and vectorB
-			float rotateAngle = Vector3.SignedAngle(vectorA.normalized, vectorB.normalized, Vector3.up);
+		// Find the angle between vectorA and vectorB
+		float rotateAngle = Vector3.SignedAngle(vectorA.normalized, vectorB.normalized, Vector3.up);
 
-			// Get the target's speed (maginitude) without the y component
-			// Only set speed factor when vector A and B are almost facing the same direction
-			float speedFactor = Vector3.Dot(vectorA, vectorB) > 0.0f ? vectorB.magnitude : 1.0f;
+		// Get the target's speed (maginitude) without the y component
+		// Only set speed factor when vector A and B are almost facing the same direction
+		float speedFactor = Vector3.Dot(vectorA, vectorB) > 0.0f ? vectorB.magnitude : 1.0f;
 
-			// Rotate towards the angle between vectorA and vectorB
-			// Use speedFactor so camera doesn't rotatate at a constant speed
-			// Limit speedFactor to be between 1 and 2
-			transform.Rotate(Vector3.up, rotateAngle * Mathf.Clamp(speedFactor, 1.0f, 2.0f) * Time.deltaTime);
+		// Rotate towards the angle between vectorA and vectorB
+		// Use speedFactor so camera doesn't rotatate at a constant speed
+		// Limit speedFactor to be between 1 and 2
+		transform.Rotate(Vector3.up, rotateAngle * Mathf.Clamp(speedFactor, 1.0f, 2.0f) * Time.deltaTime);
 
-			// Position the camera behind target at a distance of offset
-			transform.position = player.transform.position - (transform.forward * distance);
-			transform.position += Vector3.up * height;
-			//transform.LookAt(new Vector3(player.transform.position.x,transform.position.y,player.transform.position.z));
-			//transform.LookAt(player.transform.position);
-		}
-		//Smooth follow
-		else {
-
-			Transform target = player.transform;
-			float heightDamping = 4f;
-			float rotationDamping = 3.0f;
-			// Calculate the current rotation angles
-			float wantedRotationAngle = target.eulerAngles.y;
-			float wantedHeight = target.position.y + height;
-
-			float currentRotationAngle = transform.eulerAngles.y;
-			float currentHeight = transform.position.y;
-
-			// Damp the rotation around the y-axis
-			currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-		
-			// Damp the height
-			currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
-
-			// Convert the angle into a rotation
-			var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
-		
-			// Set the position of the camera on the x-z plane to:
-			// distance meters behind the target
-			transform.position = player.transform.position - (transform.forward * distance);
-			transform.position += Vector3.up * height;
-
-			// Set the height of the camera
-			transform.position = new Vector3(transform.position.x,currentHeight,transform.position.z);
-		
-			// Always look at the target
-			transform.LookAt(target);
-		}
+		// Position the camera behind target at a distance of offset
+		transform.position = player.transform.position - (transform.forward * distance);
+		transform.position += Vector3.up * height;
+		//transform.LookAt(new Vector3(player.transform.position.x,transform.position.y,player.transform.position.z));
+		//transform.LookAt(player.transform.position);
 	}
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -16,100 +16,30 @@ public class PlayerMove : MonoBehaviour
     private LayerMask ground;
 
     [SerializeField]
-    private float maxTilt = 20;
-    [SerializeField]
-    private float maxRoll = 20;
-    [SerializeField]
-    private float maxYaw = 20;
-    [SerializeField]
-    private float flyRotSpeed = 1;
-
-    [SerializeField]
-    private float flySpeedMult = 0.5f;
-    [SerializeField]
-    private float flightGrav = 1f;
-    [SerializeField]
-    private float flightLength = 10f;
-
-    private Vector3 flightPath;
-
-    [SerializeField]
-    private Animator capsuleAnimator;
-    [SerializeField]
-    private Animator  monkeyAnimator;
-
-    [SerializeField]
-    private LayerMask scoreboard;
-
-    [SerializeField]
-    private AudioSource opening;
-
+    private AudioSource collect;
     [SerializeField]
     private AudioSource rolling;
-
-    [SerializeField]
-    private AudioSource collect;
-
-    [SerializeField]
-    private GameObject water;
-
-    public bool flying;
-    public bool canScore = false;
-    public bool scored = false;
 
     private float rollInterval = 5f;
     private float rollPassed;
 
-    private bool toggle;
     // Start is called before the first frame update
     void Start()
     {
         rigid = this.GetComponent<Rigidbody>();
-        flying = false;
-        canScore = false;
-        scored = false;
-        toggle = false;
     }
 
     void Update(){       
-        if (rigid.velocity.magnitude < 0.1f && Grounded() && canScore && !scored) {
-            Score();
-        }
+
     }
 
-    public void Fly() {
-        toggle = false;
-        if (!Grounded() && !flying && !toggle) {
-            flying = true;
-            capsuleAnimator.SetTrigger("Open");
-            monkeyAnimator.SetTrigger("Fly");
-            rigid.useGravity = false;
-            transform.rotation = Quaternion.LookRotation(rigid.velocity, Vector3.up);
-            flightPath = transform.rotation.eulerAngles;
-            flightPath.x = 0;
-            rigid.angularVelocity = Vector3.zero;
-            opening.Play();
-            toggle = true;
-
-        }
-
-        if (!Grounded() && flying && !toggle) {
-            flying = false;
-            canScore = true;
-            capsuleAnimator.SetTrigger("Close");
-            monkeyAnimator.SetTrigger("StopFly");
-            rigid.useGravity = true;
-            toggle = true;
-
-        }
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         
         //Check for ground
-        if(Grounded() &! flying) {
+        if(Grounded()) {
             Vector3 floorNormal = GetFloorNormal();
             //Rolling Noise
             rollPassed += Time.deltaTime;
@@ -117,13 +47,12 @@ public class PlayerMove : MonoBehaviour
                 rolling.Play();
                 rollPassed = 0f;
             }
-            monkeyAnimator.SetFloat("WalkingMove", rigid.velocity.magnitude/10f);
              // Slow down when no input recieved
             if (SimpleInput.GetAxis("Vertical") == 0.0f && SimpleInput.GetAxis("Horizontal") == 0.0f && rigid.velocity.magnitude > 0.0f){
                 rigid.velocity = Vector3.Lerp(rigid.velocity, Vector3.zero, rollSpeed * 0.1f * Time.deltaTime);
             }
 
-            else if (!canScore) {
+            else {
                 Vector3 forward = Vector3.Cross(mainCamera.transform.right, floorNormal);
                 Vector3 forwardApply = forward * SimpleInput.GetAxis("Vertical");
                 Vector3 rightApply = SimpleInput.GetAxis("Horizontal") * mainCamera.transform.right;
@@ -133,76 +62,19 @@ public class PlayerMove : MonoBehaviour
                 
             }
         }
-        else if (flying) {
-            monkeyAnimator.SetFloat("WalkingMove", 0f);
-            float roll = -SimpleInput.GetAxis("Horizontal") * maxRoll;
-            float tilt = SimpleInput.GetAxis("Vertical") * maxTilt;
-            float yaw =  SimpleInput.GetAxis("Horizontal") * maxYaw;
-
-            flySpeedMult = Mathf.Min(flySpeedMult -(Time.deltaTime / flightLength), 0f);
-            flightGrav += Time.deltaTime / flightLength;
-
-            Vector3 targetRot = flightPath + new Vector3(tilt,yaw,roll);
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRot), Time.deltaTime * flyRotSpeed);
-
-            //Gravity
-            rigid.velocity -= Vector3.up * Time.deltaTime * flightGrav;
-
-            //Keep flying if we have some momentum (adds 'upwind')
-            Vector3 vertVel = rigid.velocity - Vector3.Exclude(transform.up, rigid.velocity);
-            rigid.velocity -= vertVel * Time.deltaTime;
-            rigid.velocity += vertVel.magnitude * transform.forward * Time.deltaTime * flySpeedMult;
-
-            //Drag
-            Vector3 forwardDrag = rigid.velocity - Vector3.Exclude(transform.forward, rigid.velocity);
-            rigid.AddForce( -forwardDrag * forwardDrag.magnitude * Time.deltaTime);
-            //High Side drag, cant glide sideways
-            Vector3 sideDrag = rigid.velocity - Vector3.Exclude(transform.right, rigid.velocity);
-            rigid.AddForce( -sideDrag * sideDrag.magnitude * Time.deltaTime);
-        }
-        else {
-            monkeyAnimator.SetFloat("WalkingMove", 0);
-        }
-
-        if (Grounded() && flying){
-            mainCamera.GetComponent<GameManager>().EndGame(false);
-        }
     }
 
     void OnTriggerEnter(Collider col)
   {
-        if (col.gameObject == water){
-            Destroy(mainCamera.GetComponent<SmoothFollow>());
-            mainCamera.GetComponent<GameManager>().EndGame(false);
-        }
 
         if (col.gameObject.tag == "pickup"){
             Destroy(col.gameObject);
-            if(flying){
-                rigid.velocity += 5 * transform.forward;
-            }
-            else{
-                Vector3 floorNormal = GetFloorNormal();
-                rigid.velocity += 5 * Vector3.Cross(mainCamera.transform.right, floorNormal);
-            }
-
+            Vector3 floorNormal = GetFloorNormal();
+            rigid.velocity += 5 * Vector3.Cross(mainCamera.transform.right, floorNormal);
             collect.Play();
-            mainCamera.GetComponent<GameManager>().score += 10;
         }
     }
     
-    public void Score() {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position,Vector3.down,out hit,2f))
-            {
-                if (scoreboard == (scoreboard | (1 << hit.transform.gameObject.layer))) 
-                {  
-                    scored = true;
-                    mainCamera.GetComponent<GameManager>().score += hit.transform.gameObject.GetComponent<Score>().score;
-                    mainCamera.GetComponent<GameManager>().EndGame(true);
-                }
-            }
-    }
     public bool Grounded()
     {
         return Physics.CheckSphere(transform.position - (Vector3.up * 0.45f), 1, ground);
