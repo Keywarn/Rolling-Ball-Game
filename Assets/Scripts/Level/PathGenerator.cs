f9ed3cb0659929b8607f777f64c721bd9daac816
Keywarn <charliehasalamb@icloud.com>
2020-12-28 15:10:55 +0000

    Spawning start path

---
+++ Assets/Scripts/Level/PathGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pathPrefab;
+    [SerializeField]
+    private int numPaths;
+
+    [SerializeField]
+    private Vector3 startPos;
+
+    private Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos;
+        for (int i = 0; i < numPaths; i++) {
+			Transform obj = (Transform)Instantiate(pathPrefab);
+			obj.localPosition = nextPos;
+			nextPos = obj.Find("End").transform.position;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
