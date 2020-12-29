201f9640d87952759a943629c574cfbdd8849db2
Keywarn <charliehasalamb@icloud.com>
2020-12-29 13:31:21 +0000

    Now triggering on exit

--- Assets/Scripts/Level/PathGenerator.cs
+++ Assets/Scripts/Level/PathGenerator.cs
@@ -13,21 +13,27 @@ public class PathGenerator : MonoBehaviour
     private Vector3 startPos;
 
     private Vector3 nextPos;
+    private Queue<Transform> pathQueue;
+    private int pathsFinished = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        pathQueue = new Queue<Transform>(numPaths);
         nextPos = startPos;
         for (int i = 0; i < numPaths; i++) {
-			Transform obj = (Transform)Instantiate(pathPrefab);
-			obj.localPosition = nextPos;
-			nextPos = obj.Find("End").transform.position;
+			Transform path = (Transform)Instantiate(pathPrefab);
+			path.localPosition = nextPos;
+			nextPos = path.Find("End").transform.position;
+            pathQueue.Enqueue(path);
 		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerExit(Collider col){
+        if (col.gameObject.tag == "pathEnd"){
+            pathsFinished += 1;
+            Destroy(col);
+            Debug.Log(pathsFinished);
+        }
     }
 }
